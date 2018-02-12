ddcda445cc06bdfc0ec3716816f139555419430f
DorraS <35916706+DorraS@users.noreply.github.com>
2018-02-12 22:59:20 +0100

    Update exercice5.php

--- exercices/variables/exercice5.php
+++ exercices/variables/exercice5.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
 * Creer un formulaire qui demande le nom de l'utilisateur
@@ -8,7 +7,6 @@
 *
 * Indice : il faut utiliser les sessions
 */
-
 session_start();
 ?>
 
@@ -19,35 +17,52 @@ session_start();
     <title>variables</title>
 </head>
 <body>
+
 <form method="post" action="exercice5.php">
-    <label for="login">nom utilisateur</label>
+    <label for="login"> saisir votre nom</label>
     <input  name="name" type="text" id="login"/><br>
     <input type="submit" value="envoyer"/>
 </form>
+
+
   
 <PRE>
+
 <?php
-   if(count($_POST)>0){
+   if(count($_POST)>0 && array_key_exists('name', $_POST)){
      $name= $_POST['name'];
      $_SESSION['name']=$name;
      echo $name;
    }
-
 ?>
+
+</PRE>
   <form method="post" action="exercice5.php">
-    <label for="prenom">saisir votre prenom</label>
+    <label for="prenom"> <?php ?>  saisir votre prenom</label>
     <input  name="first_name" type="text" id="prenom"/><br>
     <input type="submit" value="envoyer"/>
 </form>
+<PRE>
+
   <?php
-  if(count($_POST)>0){
+  if(count($_POST)>0 && array_key_exists('first_name', $_POST)){
     $first_name= $_POST['first_name'];
     $_SESSION['first_name']= $first_name;
   }
-    echo $_SESSION['name'] + '  ' + $_SESSION['first_name'];
-    var_dump($_SESSION);
+  
+    if( count($_SESSION)>0 && array_key_exists('first_name', $_SESSION) && array_key_exists('name', $_SESSION)){
+		echo $_SESSION['name'] . '  ' . $_SESSION['first_name'] .  '<br>';
+	}
+    
+    //var_dump($_SESSION);
   ?>
+  
+   <form method="post" action="exercice5.php">
+    <input  name="first_name" type="text" id="prenom"/><br>
+    <input type="submit" value="destroy session"/>
+   </form>
+  
+</pre>
   
 </body>
 </html>
-
